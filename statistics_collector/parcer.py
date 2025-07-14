8c82d934a1ae4653e6c159aa2af2d3599d86b2f9
Poryadin Artem <artem.poryadin@engee.cloud>
2025-07-14 16:30:27 +0300

    new tables

--- statistics_collector/parcer.py
+++ statistics_collector/parcer.py
@@ -1,23 +1,23 @@
 import asyncio
 from datetime import datetime
 
-from database.models import ReturnToWorkFromTests, ReturnToWorkFromReview, DevDuration, Issues1, Issues2, Issues3, Issues4
-from dependencies import client, database as db
-from utils import save_stat_record, clear_table, safe_parse_iso
+from database.models import ReturnToWorkFromTests, ReturnToWorkFromReview, DevDuration, Issues1, Issues2, Issues3, \
+    Issues4
+from dependencies import client
+from utils import save_stat_record, clear_table, safe_parse_iso, parse_dicts_from_queues
 
 
 async def parse_stat():
     await parse_all_data()
-    await generate_report_test_to_work()
-    await generate_report_to_design_review_and_back()
-    await generate_dev_duration_report()
-
+    # await generate_report_test_to_work()
+    # await generate_report_to_design_review_and_back()
+    # await generate_dev_duration_report()
 
 async def parse_all_data():
-    queue1 = (Issues1,['NWOF', 'NWOB', 'ENGEEJL', 'NWOM', 'NWOCG', 'ENGEETESTS'])
-    queue2 = (Issues2,['BUG', 'NWOBUGS'])
-    queue3 = (Issues3,['NWO'])
-    queue4 = (Issues4,['PROBLOCKS', 'BLOCKS','XBLOCKS'])
+    queue1 = (Issues1, ['NWOF', 'NWOB', 'ENGEEJL', 'NWOM', 'NWOCG', 'ENGEETEST'])
+    queue2 = (Issues2, ['BUG', 'NWOBUG'])
+    queue3 = (Issues3, ['NWO'])
+    queue4 = (Issues4, ['PROBLOCKS', 'BLOCKS', 'XBLOCKS'])
     print(f"Запущена функция: {parse_all_data.__name__}, {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
     from_date = "2025-01-01"
     to_date = datetime.now().strftime("%Y-%m-%d")
@@ -39,12 +39,12 @@ async def parse_all_data():
 
             for issue in issues:
                 issue_as_dict = {k: str(v) for k, v in issue.as_dict().items()}
-                issue_as_dict['link']=issue_as_dict['self']
-                issue_as_dict['typeOf']=issue_as_dict['type']
+                issue_as_dict['link'] = issue_as_dict['self']
+                issue_as_dict['typeOf'] = issue_as_dict['type']
                 del issue_as_dict['self']
                 del issue_as_dict['type']
+                await parse_dicts_from_queues(issue_as_dict, issue)
                 await save_stat_record(issue_as_dict, queues[0])
-
 
 
 async def generate_report_test_to_work():
@@ -91,10 +91,11 @@ async def generate_report_test_to_work():
     print(f"Завершена функция: {generate_report_test_to_work.__name__}, {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
 
 
-async def generate_report_to_design_review_and_back(csv_locale = 'sheets'):
+async def generate_report_to_design_review_and_back(csv_locale='sheets'):
     queues = ['ENGEEJL', 'NWOF', 'NWOB', 'NWOCG', 'NWOM']
 
-    print(f"Запущена функция: {generate_report_to_design_review_and_back.__name__}, {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+    print(
+        f"Запущена функция: {generate_report_to_design_review_and_back.__name__}, {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
 
     from_date = "2025-01-01"
     to_date = datetime.now().strftime("%Y-%m-%d")
@@ -115,23 +116,25 @@ async def generate_report_to_design_review_and_back(csv_locale = 'sheets'):
             changes = issue.changelog.get_all()._data
             counter = 0
 
-
             prev_status = None
             for i, change in enumerate(changes):
                 for field_change in change['fields']:
                     if not prev_status:
                         if (field_change['field'].id == 'status'
-                                and field_change['to'].key == 'dizajnrevju' if field_change['to'] else False):  # "Дизайн-ревью"
+                            and field_change['to'].key == 'dizajnrevju' if field_change[
+                            'to'] else False):  # "Дизайн-ревью"
                             prev_status = field_change['from'].key
                     else:
                         if (field_change['field'].id == 'status'):
-                            if (((field_change['from'].key == 'dizajnrevju' if field_change['from'] else False)  # "Дизайн-ревью"
-                                    and (field_change['to'].key == prev_status if field_change['to'] else False))
-                                or
-                                ((field_change['from'].key == 'dizajnrevju' if field_change['from'] else False)  # "Дизайн-ревью"
-                                    and (field_change['to'].key == 'oceredNaQa' if field_change['to'] else False)
-                                    and (prev_status == 'testing'))
-                                ):
+                            if (((field_change['from'].key == 'dizajnrevju' if field_change[
+                                'from'] else False)  # "Дизайн-ревью"
+                                 and (field_change['to'].key == prev_status if field_change['to'] else False))
+                                    or
+                                    ((field_change['from'].key == 'dizajnrevju' if field_change[
+                                        'from'] else False)  # "Дизайн-ревью"
+                                     and (field_change['to'].key == 'oceredNaQa' if field_change['to'] else False)
+                                     and (prev_status == 'testing'))
+                            ):
                                 counter += 1
                                 prev_status = None
 
@@ -146,7 +149,8 @@ async def generate_report_to_design_review_and_back(csv_locale = 'sheets'):
                 'returns_to_work_from_design_review': counter
             }, ReturnToWorkFromReview)
 
-    print(f"Завершена функция: {generate_report_to_design_review_and_back.__name__}, {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+    print(
+        f"Завершена функция: {generate_report_to_design_review_and_back.__name__}, {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
 
 
 async def generate_dev_duration_report(csv_locale=0):
@@ -190,7 +194,6 @@ async def generate_dev_duration_report(csv_locale=0):
                         end_date = safe_parse_iso(change['updatedAt'])
                         break
 
-
             duration = int((end_date - start_date).days) if start_date and end_date else ""
 
             parent_tag = issue.parent.key if issue.parent else ''
@@ -208,7 +211,6 @@ async def generate_dev_duration_report(csv_locale=0):
                 'duration': str(duration),
                 'parent_tag': parent_tag or "—"
             }, DevDuration)
-
 
     print(f"Завершена функция: {generate_dev_duration_report.__name__}, {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
 
--- statistics_collector/utils.py
+++ statistics_collector/utils.py
@@ -1,3 +1,4 @@
+import ast
 import os
 from datetime import datetime, timedelta
 import re
@@ -44,6 +45,68 @@ async def save_stat_record(data: dict, model: any) -> None:
             print("[!] Ошибка при сохранении записи:")
             print(e)
 
+async def get_projects(issue_ref):
+    changes = issue_ref.changelog.get_all()._data
+
+    return None
+
+async def parse_dicts_from_queues(issue, issue_ref):
+    issue['assignee'] = ast.literal_eval(issue['assignee'])['display'] if issue.get('assignee', None) else None
+    issue['boards'] = ', '.join([board['name'] for board in ast.literal_eval(issue['boards'])]) if issue.get('boards', None) else None
+    issue['components'] = ', '.join([component['display'] for component in ast.literal_eval(issue['components'])]) if issue.get('components', None) else None
+    issue['createdBy'] = ast.literal_eval(issue['createdBy'])['display'] if issue.get('createdBy', None) else None
+    issue['followers'] = ', '.join([follower['display'] for follower in ast.literal_eval(issue['followers'])]) if issue.get('followers',None) else None
+    issue['link'] = ('https://tracker.yandex.ru/' + issue['link'].split('/')[-1]) if issue.get('link', None) else None
+    issue['parent'] = ast.literal_eval(issue['parent'])['key'] if issue.get('parent', None) else None
+    issue['pendingReplyFrom'] = ', '.join([reply['display'] for reply in ast.literal_eval(issue['pendingReplyFrom'])]) if issue.get('pendingReplyFrom', None) else None
+    issue['tags'] = ', '.join(ast.literal_eval(issue['tags'])) if issue.get('tags', None) else None
+    issue['previousStatus'] = ast.literal_eval(issue['previousStatus'])['display'] if issue.get('previousStatus',None) else None
+    issue['previousStatusLastAssignee'] = ast.literal_eval(issue['previousStatusLastAssignee'])['display'] if issue.get('previousStatusLastAssignee', None) else None
+
+    # ToDo сделать проекты через доски
+    # issue['project'] = await get_projects(issue_ref)
+
+    issue['priority'] = ast.literal_eval(issue['priority'])['display'] if issue.get('priority', None) else None
+    issue['qaEngineer'] = ast.literal_eval(issue['qaEngineer'])['display'] if issue.get('qaEngineer', None) else None
+    issue['queue'] = ast.literal_eval(issue['queue'])['display'] if issue.get('queue', None) else None
+    issue['resolution'] = ast.literal_eval(issue['resolution'])['display'] if issue.get('resolution', None) else None
+    issue['resolvedBy'] = ast.literal_eval(issue['resolvedBy'])['display'] if issue.get('resolvedBy', None) else None
+    issue['status'] = ast.literal_eval(issue['status'])['display'] if issue.get('status', None) else None
+    issue['statusType'] = ast.literal_eval(issue['statusType'])['display'] if issue.get('statusType', None) else None
+    issue['typeOf'] = ast.literal_eval(issue['typeOf'])['display'] if issue.get('typeOf', None) else None
+    issue['updatedBy'] = ast.literal_eval(issue['updatedBy'])['display'] if issue.get('updatedBy', None) else None
+    issue['stand'] = ', '.join(ast.literal_eval(issue['stand'])) if issue.get('stand', None) else None
+    issue['developer'] = ast.literal_eval(issue['developer'])['display'] if issue.get('developer', None) else None
+
+    issue['emailTo'] = ', '.join(ast.literal_eval(issue['emailTo'])) if issue.get('emailTo', None) else None
+    issue['regress'] = ', '.join(ast.literal_eval(issue['regress'])) if issue.get('regress', None) else None
+    issue['Frontend'] = ', '.join([frontend['display'] for frontend in ast.literal_eval(issue['Frontend'])]) if issue.get('Frontend', None) else None
+
+    # issue['emailTo'] = ', '.join(ast.literal_eval(issue['emailTo'])) if issue.get('emailTo', None) else None
+    issue['local_eeEngineer'] = ast.literal_eval(issue['local_eeEngineer'])['display'] if issue.get('local_eeEngineer', None) else None
+    issue['eeEngineer'] = ast.literal_eval(issue['eeEngineer'])['display'] if issue.get('eeEngineer', None) else None
+    issue['Team'] = ', '.join([frontend['display'] for frontend in ast.literal_eval(issue['Team'])]) if issue.get('Team', None) else None
+    issue['aliases'] =  ', '.join(ast.literal_eval(issue['aliases'])) if issue.get('aliases', None) else None
+    issue['lastQueue'] = ast.literal_eval(issue['lastQueue'])['display'] if issue.get('lastQueue', None) else None
+    issue['previousQueue'] = ast.literal_eval(issue['previousQueue'])['display'] if issue.get('previousQueue', None) else None
+    issue['Size'] = ', '.join(ast.literal_eval(issue['Size'])) if issue.get('Size', None) else None
+
+async def get_duration(changes):
+    start_date = None
+    end_date = None
+    for change in changes:
+        for field_change in change['fields']:
+            if field_change['field'].id != 'status':
+                continue
+
+            to_status = field_change['to'].key if field_change['to'] else ''
+            if not start_date and to_status == 'inProgress':  # "В РАБОТЕ"
+                start_date = safe_parse_iso(change['updatedAt'])
+            elif not end_date and to_status == 'closedDev':  # "Готово - Есть на Dev"
+                end_date = safe_parse_iso(change['updatedAt'])
+                break
+
+    duration = int((end_date - start_date).days) if start_date and end_date else ""
 
 def run_migrations():
     # logger.info("Запускаем миграции")
